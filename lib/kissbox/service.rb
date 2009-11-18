61d0a228c7123676c3fa3f30a4723ab18fe492b0
dirk luesebrink <dirk@sebrink.de>
2009-11-18 16:19:15 +0100

    working duft 2 sequence

--- lib/kissbox/service.rb
+++ lib/kissbox/service.rb
@@ -1,16 +1,27 @@
 module Kissbox
 
   class Service < Gom::Remote::Entry
-    
+
     Defaults = { }
 
     attr_reader :path
 
+    class << self
+      def oattr *names
+        names.each do |name|
+          class_eval "def #{name}; @options['#{name}'.to_sym]; end"
+        end
+      end
+    end
+    oattr :kissbox_ip, :kissbox_port
+
     def initialize path, options = {}
       @path = path
-      @options = (Defaults.merge options)
+      #@options = (Defaults.merge options)
+      @options = Defaults.merge(gnode(@path)).merge(options)
+      puts "options: #{@options.inspect}"
 
-      @chan = TcpMode.new
+      @chan = TcpMode.new self
 
       #@values_sub = Subscription.new(
       #  "#{@path}/values", 
@@ -22,6 +33,21 @@ module Kissbox
 
     def push smell
       @chan.push smell
+    end
+
+    # TODO: stuff below belongs to gom-script
+    private
+
+    def gnode path
+      json = (Gom::Remote.connection.read "#{path}.json")
+      (JSON.parse json)["node"]["entries"].select do |entry|
+        # 1. select attribute entries
+        entry.has_key? "attribute"
+      end.inject({}) do |h, a|
+        # 2. make it a key, value list
+        h[a["attribute"]["name"].to_sym] = a["attribute"]["value"]
+        h
+      end
     end
   end
 end
--- lib/kissbox/tcp-mode.rb
+++ lib/kissbox/tcp-mode.rb
@@ -1,10 +1,44 @@
+require 'socket'
+
 module Kissbox
   class TcpMode
 
-    def push smell
-      puts " -- pushing smell: #{smell}"
+    Sequences = {
+      :duft2 => [
+        {:wait => 0, :cage => 2, :slot => 1, :state => :on},
+        {:wait => 1, :cage => 1, :slot => 1, :state => :on},
+        {:wait => 1, :cage => 1, :slot => 4, :state => :on},
+        {:wait => 7, :cage => 1, :slot => 4, :state => :off},
+        {:wait => 1, :cage => 1, :slot => 1, :state => :off},
+        {:wait => 2, :cage => 2, :slot => 1, :state => :off},
+      ]
+    }
+
+    def initialize service
+      @service = service
     end
 
-    private
+
+    def push smell
+      puts " -- pushing smell: #{smell}, target: #{@service.kissbox_ip}:#{@service.kissbox_port}"
+      sock = TCPSocket.new(@service.kissbox_ip, @service.kissbox_port)
+
+      (steps = Sequences[smell.to_sym]) or (raise "no such smell: #{smell}")
+      steps.each do |step|
+        sleep step[:wait]
+        puts "step: #{step.inspect}"
+        write_single_channel_value step[:cage], step[:slot], step[:state], sock
+      end
+    ensure
+      sock.close rescue nil
+    end
+
+    def write_single_channel_value cage, slot, state, sock = nil
+      sock ||= TCPSocket.new(@service.kissbox_ip, @service.kissbox_port)
+      msg = [0xA5, cage-1, slot-1, (state == :on ? 0x01 : 0x00)].pack "C*"
+      sock.write msg
+      sock.write msg if(state == :off)
+      #puts "written: #{msg}"
+    end
   end
 end
