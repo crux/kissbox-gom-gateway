eb1d1e75a6478d71f1c10fc781a3b2476a0f83b7
dirk luesebrink <dirk@sebrink.de>
2009-11-19 10:12:31 +0100

    working push sequences
    it took a while to figure out the how to please the fricking kissbox. The
    box drops TCP messages when the come to fast. I added a 100ms sleep time
    after every message and the simulator works.

--- lib/kissbox/tcp-mode.rb
+++ lib/kissbox/tcp-mode.rb
@@ -4,14 +4,54 @@ module Kissbox
   class TcpMode
 
     Sequences = {
-      :duft2 => [
-        {:wait => 0, :cage => 2, :slot => 1, :state => :on},
-        {:wait => 1, :cage => 1, :slot => 1, :state => :on},
-        {:wait => 1, :cage => 1, :slot => 4, :state => :on},
-        {:wait => 7, :cage => 1, :slot => 4, :state => :off},
-        {:wait => 1, :cage => 1, :slot => 1, :state => :off},
-        {:wait => 2, :cage => 2, :slot => 1, :state => :off},
-      ]
+      :one_one_on  => [ {:wait => 0, :cage => 1, :slot => 1, :state => :on} ],
+      :one_one_off => [ {:wait => 0, :cage => 1, :slot => 1, :state => :off} ],
+      :two_one_on =>  [ {:wait => 0, :cage => 2, :slot => 1, :state => :on} ],
+      :two_one_off => [ {:wait => 0, :cage => 2, :slot => 1, :state => :off} ],
+      :two_one_off => [ {:wait => 0, :cage => 2, :slot => 1, :state => :off} ],
+
+      :on => [
+        {:wait =>  0.0, :cage => 1, :slot => 1, :state => :on},
+        {:wait =>  0.2, :cage => 1, :slot => 2, :state => :on},
+        {:wait =>  0.2, :cage => 1, :slot => 3, :state => :on},
+        {:wait =>  0.2, :cage => 1, :slot => 4, :state => :on},
+        {:wait =>  0.2, :cage => 2, :slot => 1, :state => :on},
+    ],
+
+    :off => [
+      {:wait =>  0.0, :cage => 1, :slot => 1, :state => :off},
+      {:wait =>  0.2, :cage => 1, :slot => 2, :state => :off},
+      {:wait =>  0.2, :cage => 1, :slot => 3, :state => :off},
+      {:wait =>  0.2, :cage => 1, :slot => 4, :state => :off},
+      {:wait =>  0.2, :cage => 2, :slot => 1, :state => :off},
+    ],
+
+    :duft1 => [
+      {:wait =>  0.0, :cage => 2, :slot => 1, :state => :on},
+      {:wait =>  1.0, :cage => 1, :slot => 1, :state => :on},
+      {:wait =>  1.0, :cage => 1, :slot => 4, :state => :on},
+      {:wait => 15.0, :cage => 1, :slot => 4, :state => :off},
+      {:wait =>  1.0, :cage => 1, :slot => 1, :state => :off},
+      {:wait =>  2.0, :cage => 2, :slot => 1, :state => :off},
+    ],
+    :duft2 => [
+      {:wait => 0.0, :cage => 2, :slot => 1, :state => :on},
+      {:wait => 1.0, :cage => 1, :slot => 1, :state => :on},
+      {:wait => 1.0, :cage => 1, :slot => 4, :state => :on},
+      {:wait => 7.0, :cage => 1, :slot => 4, :state => :off},
+      {:wait => 1.0, :cage => 1, :slot => 1, :state => :off},
+      {:wait => 2.0, :cage => 2, :slot => 1, :state => :off},
+    ],
+    :duft3 => [
+      {:wait =>  0.0, :cage => 2, :slot => 1, :state => :on},
+      {:wait =>  1.0, :cage => 1, :slot => 1, :state => :on},
+      {:wait =>  0.1, :cage => 1, :slot => 2, :state => :on},
+      {:wait =>  1.0, :cage => 1, :slot => 4, :state => :on},
+      {:wait => 10.0, :cage => 1, :slot => 4, :state => :off},
+      {:wait =>  1.0, :cage => 1, :slot => 2, :state => :off},
+      {:wait =>  0.1, :cage => 1, :slot => 1, :state => :off},
+      {:wait =>  2.0, :cage => 2, :slot => 1, :state => :off},
+    ],
     }
 
     def initialize service
@@ -30,6 +70,7 @@ module Kissbox
         write_single_channel_value step[:cage], step[:slot], step[:state], sock
       end
     ensure
+      sock.flush rescue nil
       sock.close rescue nil
     end
 
@@ -37,7 +78,12 @@ module Kissbox
       sock ||= TCPSocket.new(@service.kissbox_ip, @service.kissbox_port)
       msg = [0xA5, cage-1, slot-1, (state == :on ? 0x01 : 0x00)].pack "C*"
       sock.write msg
-      sock.write msg if(state == :off)
+      #if(state == :off)
+      #  10.times { sock.write msg }
+      #end
+    ensure 
+      sock.flush
+      sleep 0.1
       #puts "written: #{msg}"
     end
   end
